e92f3d0d0f764d114ba2e3ef25576e642378ab45
harvestdor-dev lyberadmin <lyberadmin@harvestdor-dev.stanford.edu>
2013-02-06 14:15:25 -0800

    make delete_coll.sh harder to mess things up by accident

--- bin/delete_coll.sh
+++ bin/delete_coll.sh
@@ -1,1 +1,2 @@
-curl "https://sul-solr-test.stanford.edu/solr/mods_profiler/update?commit=true" -H "Content-Type: application/xml" --data-binary '<delete><query>collection:bnf_images</query></delete>'
+# change value below to YOUR collection
+curl "https://sul-solr-test.stanford.edu/solr/mods_profiler/update?commit=true" -H "Content-Type: application/xml" --data-binary '<delete><query>collection:yer_coll</query></delete>'

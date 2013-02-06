f27731317fc3c274050cd59974904c3b8ffa0055
harvestdor-dev lyberadmin <lyberadmin@harvestdor-dev.stanford.edu>
2013-02-06 14:14:13 -0800

    fix bin/delete_coll.sh

--- bin/delete_coll.sh
+++ bin/delete_coll.sh
@@ -1,1 +1,1 @@
-curl "https://sul-solr-test.stanford.edu/solr/mods_profiler?commit=true" -H "Content-Type: application/xml" --data-binary '<delete><query>collection:bnf_images</query></delete>'
+curl "https://sul-solr-test.stanford.edu/solr/mods_profiler/update?commit=true" -H "Content-Type: application/xml" --data-binary '<delete><query>collection:bnf_images</query></delete>'
